19f90048383faed126b5a090115786388c5df219
RealSoSity <realsosity@gmail.com>
2025-09-29 14:30:45 +0700

    Add Icon Picker complete

---
+++ frontend/testsvelte/src/lib/components/EmojiTab.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    const emojiList = ['🍔','🍕','🍣','🍜','🚗','🏠','💡','❤️','🎁','📚','💼','💳','📈','🚌'];
+
+    let {onPick = (value:string) => {}} = $props();
+</script>
+
+<div class="grid grid-cols-8 gap-3">
+    {#each emojiList as e}
+        <button type="button" class="border rounded-lg p-3 hover:bg-gray-100 hover:cursor-pointer" onclick={() => onPick(e)}>
+            {e}
+        </button>
+    {/each}
+</div>
