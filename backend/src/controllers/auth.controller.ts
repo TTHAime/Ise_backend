85f5e3ef1139f0ed07e0c089bc424798dac45698
Time_thp <thanaphon.thpcd@gmail.com>
2025-10-16 15:35:03 +0700

    fix redirect url error

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -158,5 +158,5 @@ export const googleAuthCallbackHandler = catchErrors(async (req, res) => {
   clearOAuthStateCookie(res);
   setAuthCookie({ res, accessToken, refreshToken });
 
-  return res.redirect(`${config.APP_ORIGIN}home`);
+  return res.redirect(`${config.APP_ORIGIN}/home`);
 });
